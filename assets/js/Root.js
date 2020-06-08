7579a92030593dcdd04eaa72ea4c5a907ad6264c
Benjamin De La Torre <bdelatorre@gozego.com>
2020-06-08 16:35:02 -0700

    --wip-- [skip ci]

---
+++ assets/js/Root.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react';
+
+class Root extends Component {
+  render() {
+    return (
+      <div>
+        <p>Test Paragraph</p>
+      </div>
+    );
+  }
+}
+
+export default Root;
