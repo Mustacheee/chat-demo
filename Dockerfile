14a9d236b6f3c8822eb3be8f451afcf54894a0b7
Benjamin De La Torre <bendelatorre@ymail.com>
2020-06-22 15:55:19 -0700

    --wip-- [skip ci]

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM elixir:1.8.2
 
 ENV MIX_ENV dev
 ENV NODE_ENV development
-ENV PORT 4001
-EXPOSE 4001
+ENV PORT 4000
+EXPOSE 4000
 
 RUN apt-get update
 RUN apt-get install -y postgresql-client inotify-tools
@@ -12,7 +12,7 @@ RUN mix local.hex --force
 RUN mix local.rebar --force
 RUN mix archive.install --force http://github.com/phoenixframework/archives/raw/master/phx_new-1.3.0.ez
 
-RUN curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
+RUN curl -sL https://deb.nodesource.com/setup_13.x -o nodesource_setup.sh
 RUN bash nodesource_setup.sh
 RUN apt-get install nodejs
 
---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Docker entrypoint script.
+
+mix deps.get
+npm install 
+
+# Wait until Postgres is ready
+while ! pg_isready -q -h $PGHOST -p $PGPORT -U $PGUSER
+do
+  echo "$(date) - waiting for database to start"
+  sleep 2
+done
+
+echo "Looking for database"
+
+# Create, migrate, and seed database if it doesn't exist.
+if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
+  echo "Database $PGDATABASE does not exist. Creating..."
+  mix ecto.setup
+  echo "Database $PGDATABASE created."
+else
+  echo "Database already exists"
+fi
+
+echo "Starting application"
+exec mix phx.server
